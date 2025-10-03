b78bf80e290b9ef4f67737184044ac99a9faf020
Saman Sathenjeri <saman.m.sath@gmail.com>
2025-10-03 17:43:06 -0400

    fix: multibar progress w/ status updates
    Signed-off-by: Saman Sathenjeri <saman.m.sath@gmail.com>

--- spnl/src/pull.rs
+++ spnl/src/pull.rs
@@ -1,5 +1,6 @@
 use fs4::fs_std::FileExt;
 use futures::stream::StreamExt;
+use std::collections::HashMap;
 use indicatif::{MultiProgress, ProgressBar, ProgressStyle};
 use tokio::io::{AsyncBufReadExt, BufReader};
 
@@ -49,6 +50,7 @@ struct PullRequest {
 #[derive(Debug, serde::Deserialize)]
 struct PullResponse {
     status: String,
+    digest: Option<String>,
     total: Option<u64>,
     completed: Option<u64>,
 }
@@ -70,12 +72,6 @@ async fn ollama_pull_if_needed(model: &str) -> anyhow::Result<()> {
         /*f.lock_exclusive()?;
         if !ollama_exists(model).await?*/
         {
-            // create new MultiProgress bar
-            let m = MultiProgress::new();
-            let style = ProgressStyle::with_template(
-                "{msg} {wide_bar:.cyan/blue} {pos:>7}/{len:7} [{elapsed_precise}]",
-            )?;
-
             // creating client and request body
             let http_client = reqwest::Client::new();
             let request_body = PullRequest {
@@ -103,43 +99,65 @@ async fn ollama_pull_if_needed(model: &str) -> anyhow::Result<()> {
             let buf_reader = BufReader::new(stream_reader);
             let mut lines = buf_reader.lines();
 
-            let pb = m.add(ProgressBar::new(0));
-            pb.set_style(style);
+            // creation of multiprogress container and style
+            let m = MultiProgress::new();
+            let style =
+                ProgressStyle::with_template("{msg:<20} {percent:>3}% ▕{wide_bar}▏ {bytes:>7}")
+                    .expect("Failed to create progress style template")
+                    .progress_chars("█ ");
+            let mut digests: HashMap<String, ProgressBar> = HashMap::new();
+            let mut final_status_lines: Vec<String> = Vec::new();
 
             while let Some(line) = lines.next_line().await? {
                 // stores in pull response struct
                 let update: PullResponse = match serde_json::from_str(&line) {
                     Ok(u) => u,
                     Err(e) => {
-                        return Err(anyhow::anyhow!("Invalid JSON in PullResponse: {e}"));
+                        eprintln!("Failed to map JSON into PullResponse: {e}");
+                        return Err(anyhow::anyhow!("Ollama API request failed"));
                     }
                 };
 
-                // checks for error or end of stream
-                if update.status.to_lowercase() == "error" {
-                    pb.finish_and_clear();
-                    FileExt::unlock(&f)?;
-                    return Err(anyhow::anyhow!("Ollama streaming error: "));
-                } else if update.status.to_lowercase() == "success" {
-                    pb.finish_with_message(format!("Model {model} pulled successfully"));
-                    break;
+                let my_status = update.status.to_lowercase();
+
+                if let Some(digest) = update.digest {
+                    // handles multiple progress bars
+                    let current_pb = digests.entry(digest.clone()).or_insert_with(|| {
+                        let new_pb = m.add(ProgressBar::new(0));
+                        new_pb.set_style(style.clone());
+                        new_pb
+                    });
+
+                    current_pb.set_message(my_status.clone());
+
+                    // sets progress bar length
+                    if let (Some(total), Some(done)) = (update.total, update.completed) {
+                        if current_pb.length().unwrap_or(0) == 0 {
+                            current_pb.set_length(total);
+                        }
+                        current_pb.set_position(done);
+                    }
+                } else if digests.is_empty() { // prints out status updates (before download)
+                    m.println(&my_status).unwrap();
+                } else { // stores to print out status updates (after download)
+                    final_status_lines.push(my_status.clone());
                 }
 
-                // sets progress bar length
-                pb.set_message(update.status.to_lowercase());
-                if let (Some(total), Some(done)) = (update.total, update.completed) {
-                    if total == done {
-                        pb.set_length(total);
-                        pb.set_position(done);
-                    }
-                    if pb.length().unwrap_or(0) == 0 {
-                        pb.set_length(total);
-                    }
-                    pb.set_position(done);
+                // checks for error or end of stream
+                if my_status == "error" {
+                    return Err(anyhow::anyhow!("Ollama streaming error: {}", line));
+                } else if my_status == "success" {
+                    break;
                 }
             }
-            FileExt::unlock(&f)?;
+
+            // finishes drawing progress bars and outputs rest of status updates
+            m.set_draw_target(indicatif::ProgressDrawTarget::hidden());
+            for line in final_status_lines {
+                println!("{}", line);
+            }
         }
+        FileExt::unlock(&f)?;
     }
 
     Ok(())
