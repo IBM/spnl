ca9fc65aa65bf749a2d0198fc9db1fdb56df92b3
Nick Mitchell <nickm@us.ibm.com>
2026-02-16 08:52:31 -0500

    chore: bump to llmd 0.5.0
    - Increase dev mode GCE disk to 300GB pd-balanced for compilation
    - Keep production mode at 100GB pd-ssd for optimal serving performance
    - Fixes exit code 128 failures due to insufficient disk space during Rust compilation
    
    Signed-off-by: Nick Mitchell <nickm@us.ibm.com>

--- docker/vllm/llm-d/clone.sh
+++ docker/vllm/llm-d/clone.sh
@@ -2,12 +2,14 @@
 
 set -e
 
-LLMD_VERSION=0.4.0
-BASE_VLLM_FORK=https://github.com/neuralmagic/vllm.git
-BASE_VLLM_BRANCH=llm-d-release-0.4
+LLMD_VERSION=0.5.0
+BASE_VLLM_FORK=https://github.com/vllm-project/vllm.git
+BASE_VLLM_COMMIT_SHA=d7de043d55d1dd629554467e23874097e1c48993
 
-git clone $BASE_VLLM_FORK -b $BASE_VLLM_BRANCH --depth 1
+git clone $BASE_VLLM_FORK vllm
 cd vllm
+git fetch --depth=1 origin $BASE_VLLM_COMMIT_SHA
+git checkout -q $BASE_VLLM_COMMIT_SHA
 
 for patchfile in ../patches/$LLMD_VERSION/*.patch.gz
 do git apply <(gunzip -c $patchfile) --reject
